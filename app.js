43964f813ac7416da1fcf2229dfcc55b345e9eb5
osamamagableh <osama.magableh@gmail.com>
2021-03-04 13:56:36 +0200

    My Java test

---
+++ app.js
@@ -0,0 +1,19 @@
+alert("Hello");
+var name = prompt("Enter your Name ");
+if (name == "Osama" )
+{
+alert("You are Admin");
+}
+else  
+{
+ document.write("Welcome  "+name );
+}
+
+console.log("Hi from Osama");
+console.log("Code 102");
+
+var a = 5;
+var b= 10;
+var c= a+b;
+console.log( a>b || a!=b );
+console.log(a>a);
